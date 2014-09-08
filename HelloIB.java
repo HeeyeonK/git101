0a54018bac959468c4fb48b5ddbfac9604f4990a
Mr.M <morrisv@harrisoncsd.org>
2014-09-08 12:40:49 -0400

    initial commit

---
+++ HelloIB.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class HelloIB here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloIB
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class HelloIB
+     */
+    public HelloIB()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
